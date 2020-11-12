8c3696856b5a0a40871a69522700af98f19fa6dc
Okan Gul <okan.gul@icloud.com>
2020-11-12 11:36:42 -0500

    initial commit

---
+++ tasks/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+class Task(TimeStampedModel):
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
---
+++ tasks/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from .models import *
+from .forms import *
+
+def index(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'tasks/list.html', context)
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+    context = {'form': form}
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+
+    return render(request, 'tasks/update_task.html', context)
+
+def delete_task(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item': item}
+    return render(request, 'tasks/delete.html', context)
