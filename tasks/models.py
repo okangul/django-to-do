c216e7ac9392f4ff725e1d12d5bfaf353575e263
Okan Gul <okan.gul@icloud.com>
2020-11-13 22:12:51 -0500

    Form field naming changed

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
+
 class Task(TimeStampedModel):
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
